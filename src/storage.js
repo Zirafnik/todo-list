c7136096da959fc76c670cc8ec9c29065b2eaf1d
Zirafnik <dh@ptica.com>
2021-02-03 18:14:40 +0100

    added local storage

--- src/storage.js
+++ src/storage.js
@@ -1,10 +1,30 @@
 let projectStorage= [];
 
+//LOCAL STORAGE
+//check if exists
+function getLocalStorage() {
+    if (typeof(Storage) !== "undefined" && window.localStorage.length!=0) {
+        projectStorage= JSON.parse(window.localStorage.getItem('projectsData'));
+        } else {
+        console.log('no storage support');
+    }
+}
+
+function updateLocalStorage() {
+    window.localStorage.clear();
+    window.localStorage.setItem('projectsData', JSON.stringify(projectStorage));  //JSON.stringify enumerates propreties, so it will not include non-enumerable
+}   
+
+
 function saveProjectToStorage(project) {
     projectStorage.push(project);
     console.log(projectStorage);
+
+    updateLocalStorage();
 }
 
-//change task propreties**
+function deleteProject() {
+
+}
 
 export {saveProjectToStorage, projectStorage};
